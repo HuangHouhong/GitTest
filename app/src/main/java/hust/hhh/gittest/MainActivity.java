96c0dbf22a88aa310511b3f5c2d31c800ff64354
HuangHouhong <985956005@qq.com>
2019-01-06 13:34:23 +0800

    add ignore
    Signed-off-by: HuangHouhong <985956005@qq.com>

--- app/src/main/java/hust/hhh/gittest/MainActivity.java
+++ app/src/main/java/hust/hhh/gittest/MainActivity.java
@@ -2,6 +2,7 @@ package hust.hhh.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("HHH" ,"添加一点文件，测试一下咯");
     }
 }
